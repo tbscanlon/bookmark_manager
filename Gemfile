980e4ac8409dd77bdbf0d3ebfa07f808ea0f4a3d
tbscanlon <thomas.scanlon@outlook.com>
2017-06-27 15:24:18 +0100

    Clean Gemfile

--- Gemfile
+++ Gemfile
@@ -7,11 +7,10 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'dm-transactions'
 gem 'sinatra'
+gem 'database_cleaner'
 
-# gem "rails"
 group :test do
   gem 'capybara'
-  gem 'database_cleaner'
   gem 'rspec'
   gem 'selenium-webdriver'
 end
--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,9 @@
 ENV['RACK_ENV'] ||= "development"
+
 require 'sinatra/base'
 require_relative './models/link'
 
 class BookmarkManager < Sinatra::Base
-
-  # ENV['RACK_ENV'] ||= "development"
-
   get '/links' do
     @links = Link.all
     erb :links
