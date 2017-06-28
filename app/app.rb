2d644f8aee1747c790e51c22fcf1c218c4948dbf
tbscanlon <thomas.scanlon@outlook.com>
2017-06-28 14:13:55 +0100

    Implement multiple tags for links

--- app/app.rb
+++ app/app.rb
@@ -23,16 +23,24 @@ class BookmarkManager < Sinatra::Base
 
   end
 
+  get '/links/add_tag' do
+    erb :add_tag
+  end
+
+  post '/links/add_tag' do
+    link = Link.first(title: params[:website])
+    tag = Tag.first_or_create(name: params[:add_tag])
+    link.tags << tag
+    link.save
+    p link.tags
+    redirect '/links'
+  end
+
   get '/tags/:search' do
     tag = Tag.first(name: params[:search])
     @links = tag.links
     erb :search
-
-
-
-
   end
-
 
   run! if app_file == $PROGRAM_NAME
 end
--- spec/feature/creating_tags_spec.rb
+++ spec/feature/creating_tags_spec.rb
@@ -1,10 +1,24 @@
 require "./app/models/link.rb"
 
 feature "tagging links" do
-  scenario "I can tag links in my bookmark manager" do
+  before(:each) do
+    Tag.create(name: "Sport")
+    Tag.create(name: "Shopping")
     add_link(url: "http://www.facebook.com", title: "Facebook", tag: "Social Media")
+  end
 
+  scenario "I can tag links in my bookmark manager" do
     link = Link.first
     expect(link.tags.map(&:name)).to include('Social Media')
+  end
+
+  scenario "Adding tags to existing links" do
+    visit '/links/add_tag'
+    fill_in(:website, with: "Facebook")
+    fill_in(:add_tag, with: "Fake News")
+    click_button("add")
+
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('Social Media').and include('Fake News')
   end
 end
